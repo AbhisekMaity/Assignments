31751bada642521820ddedce952524e1ad7768f4
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-30 12:27:57 +0530

    Add files via upload

---
+++ Statements_In_Java_Activity_1 (1)/Shapes.java
@@ -0,0 +1,41 @@
+package com.cognizant.shapes;
+
+  public class Shapes {
+   int noOfSides;
+    public void calculateShapeArea(String shape,int noOfSides) {
+    	this.noOfSides=noOfSides;
+    }
+    public static void main(String[] args) {
+    	
+    	Shapes sph = new Shapes();
+    	sph.noOfSides=9;
+    	
+    	Circle2 cir=new Circle2();
+    	Triangle2 tir = new Triangle2();
+    	Squre2 sqr = new Squre2();
+    	
+    	cir.Radius=5;
+    	tir.Sides=6;
+    	sqr.Side=8;
+    	
+    	if(sph.noOfSides==1) {
+         System.out.println("The area of circle is " + cir.calculateArea());
+     }
+
+    	else if(sph.noOfSides==3) {
+         System.out.println("The area of circle is " + tir.calculateArea());
+     }
+
+    	else if(sph.noOfSides==4) {
+         System.out.println("The area of circle is " + sqr.calculateArea());
+     }
+    	else {
+            System.out.println("No shapes present ");
+
+    	}
+    	
+    	
+    
+		
+	}
+}
---
+++ Statements_In_Java_Activity_1 (1)/Triangle2.java
@@ -0,0 +1,8 @@
+package com.cognizant.shapes;
+
+public class Triangle2 {
+  int Sides;
+  public double calculateArea() {
+	  return 0.433*Sides*Sides;
+  }
+}
