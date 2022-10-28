09f2778965462f614a9b85f77edb27d3af3d048a
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-10-28 12:17:28 +0530

    Add files via upload

---
+++ Lab session 28th oct 2544/MainMovie.java
@@ -0,0 +1,17 @@
+package com.Assignments;
+
+public class MainMovie {
+  public static void main(String[] args) {
+	  Movie obj = new Movie(12,3,500);
+	  obj.setMovieName("KGF chapter 2");
+	  System.out.println("The movie name is "+obj.getMovieName());
+	  System.out.println("The movie start time "+obj.movieStartTime);
+	  System.out.println("The movie start time "+obj.movieEndTime);
+	  System.out.println("The movie ticket price= "+obj.movieTicketPrice);
+
+	  
+	  
+	  
+	  
+}
+}
---
+++ Lab session 28th oct 2544/Movie.java
@@ -0,0 +1,45 @@
+package com.Assignments;
+/*
+ * Problem 2
+Movie Theatre Management System, where you can create two classes one is main another one is movie, 
+movie class can have two methods setMovie,getMovie where you can create movieName,movieStartTime,movieEndTime,
+MovieTicketPrice as instance variables
+
+
+
+ */
+public class Movie {
+  String movieName;
+  int movieStartTime;
+  int movieEndTime;
+  double movieTicketPrice;
+  Movie(int movieStartTime,int movieEndTime,double movieTicketPrice){
+	  this.movieStartTime=movieStartTime;
+	  this.movieEndTime=movieEndTime;
+	  this.movieTicketPrice=movieTicketPrice;
+  }
+public String getMovieName() {   // we get movie name 
+	return movieName;
+}
+public void setMovieName(String movieName) {  // here we set movie name 
+	this.movieName = movieName;
+}
+//public int getMovieStartTime() {
+//	return movieStartTime;
+//}
+//public void setMovieStartTime(int movieStartTime) {
+//	this.movieStartTime = movieStartTime;
+//}
+//public int getMovieEndTime() {
+//	return movieEndTime;
+//}
+//public void setMovieEndTime(int movieEndTime) {
+//	this.movieEndTime = movieEndTime;
+//}
+//public double getMovieTicketPrice() {
+//	return movieTicketPrice;
+//}
+//public void setMovieTicketPrice(double movieTicketPrice) {
+//	this.movieTicketPrice = movieTicketPrice;
+//}
+}
